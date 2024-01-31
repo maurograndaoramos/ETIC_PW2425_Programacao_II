9d381fb84057288eb8c265fc446798ca74c8b252
maurograndaoramos <mauro_ramos@eticalgarve.com>
2024-01-31 21:55:24 +0000

    Adding Aula 9 (some changes) and importing 10 thru 12

--- Aula9/app.js
+++ Aula9/app.js
@@ -1,45 +1,61 @@
-console.log("1");
+`
+criar uma função que receba 2 parametros (a, b)
+o resultado desta função é a soma destes 2 parametros (a, b);
 
-window.onload = () => {
-    console.log("document loaded, applicaton started");
-};
+chamam a função e façam console.log() do resultado.
+`
 
-console.log ("2");
 
+//criar funçao que receba 2 parametros (a,b)
 const addNumbers = (a,b) => {
-    return a + b
+    return a + b;
 }
 
-a = 2
-b = 3
+//chamar a função e guardar o resultado numa variavel
+const result = addNumbers(1,2);
 
-const result = addNumbers (a,b)
+//fazer console.log dessa variavel
+// console.log(result);
 
-console.log(result)
+`
+criar outra funcao que receba 2 parametros (c, d)
+o resultado desta funcao é a multiplicação destes 2 parametros.
+cahmar a funcao e guardar o resultado numa variavel
+mostrar a variavel em console.log
+`
 
-
-
-const multiplyNumbers = (c, d) => {
-    return c * d; 
+//criar função que multiplique 2 parametros (c,d)
+const mulNumbers = (c,d) => {
+    return c * d;
 }
 
-const resultMultiply = multiplyNumbers(2, 3)
+//chamar função e guardar resultado em variavel
+const result2 = mulNumbers(3, 2);
 
-console.log(resultMultiply);
+//mostar variavel em console log
+// console.log(result2);
 
 
-const multFunctions = (e, f) => {
-    return e * f;
-}
+`
+multiplicar o resultado da primeira funçao (addNumbers(a,b)) por 2
+guardar o resultado numa variavel e fazer console.log dela
+`
 
-const resultsFunctionMultiply = multFunctions(result, 2)
+// Multiplcar resultado de addNumbers por 2
+const result3 = mulNumbers(result, 2);
 
-console.log(resultsFunctionMultiply)
+//mostrar resultado em console
+// console.log(result3);
 
-console.log(multiplyNumbers(addNumbers(2, 3), 2))
+// console.log(mulNumbers(addNumbers(1,2), 2));
 
-const array = [2, 45, 34, 2]
+`
+criar uma funçao que receba um array como parametro, e que calcule a media dos numeros desse array
+mostrar o resulto no console.log
+`
+const arr = [2, 45, 34, 2, 54, 2000, 23.5];
 
+//criar função que calcule a media do array
 const calcAverage = (nums) => {
 
     let value = 0;
@@ -50,37 +66,65 @@ const calcAverage = (nums) => {
     return value / nums.length;
 }
 
-const result4 = calcAverage(array);
-console.log(result4)
+//mostrar o resultado em console.log
 
-const array2 = [2, 3, 4, 5]
+const result4 = calcAverage(arr);
+// console.log(result4);
 
-array2.forEach((element) => {
-    console.log(element)
- });
+`criar um array, iterar sobre ele e mostrar os items 1 a 1 pela console.log`
 
+const arr2 = [12, 45, 34, 89, 100, 23452];
 
+arr2.forEach(num => {
+    console.log(num);
+});
+
+console.log("----");
+
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]);
+}
+
+console.log("----");
 let end = false;
 let index = 0;
 while (!end) {
-    console.log("------")
-    console.log(array2[index])
+    console.log(arr2[index]);
     index++;
-    end = index >= array2.length;
-};
+    end = index >= arr2.length;
+}
 
+console.log("----");
+const obj = {
+    name: "bruno",
+    age: 32,
+    gender: "male"
+}
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
 
+console.log("----");
+for (const value of arr2) {
+    console.log(value);
+}
+
+`criar uma classe com o nome Test, essa classe tem um metodo que retorna o numero 2`
+
+//Criar a classe Test
+//criar o metodo que retorne o numero 2, guardar o resultado numa variavel
 class Test {
     constructor() {
 
     }
-    
-    showNumber () {
-        return 2
+    showNumber() {
+        return 2;
     }
-
-
 }
 
-console.log("------")
-console.log(new Test().showNumber())
+console.log("----");
+console.log(new Test().showNumber());
+
+
+//fazer console log desse resultado (variavel)
+
